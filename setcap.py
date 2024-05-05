75d3cfca55c3a8a633c5606f38346e6a705ca764
jeet12 <rajkumar.soulrider.jeet47@gmail.com>
2024-05-05 20:00:42 +0530

    Update setcap.py

--- setcap.py
+++ setcap.py
@@ -8,7 +8,7 @@ def main():
     # Step 1: Copy python3 to /home/demo/
     subprocess.run(["cp", source_path, destination_path], check=True)
 
-    # Step 2: Set capabilities cap_setuid+ep for /home/demo/python3
+     # Step 2: Set capabilities cap_setuid+ep for /home/demo/python3
     subprocess.run(["setcap", "cap_setuid+ep", destination_path], check=True)
 
 if __name__ == "__main__":
